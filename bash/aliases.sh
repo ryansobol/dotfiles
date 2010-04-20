a2ece67fcbacfc201160b08098c6c40bed184991
Ryan Sobol <code@ryansobol.com>
2010-04-20 16:09:05 -0500

    bash/aliases.sh   * Moved reload to the top for better visibility   * Renamed gems to cdgems   * Renamed gems1.9 to cdgems1.9   * The above effects consolodated the shell aliases into tar aliases   * Added tm and tu bash/config.sh   * Commentted in my prompt   * Adapted to an Ubunty-style prompt but incorporated vcprompt (experimental)   * Commentted out Peepcode's prompt (would like to merge at some point in the future)

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,3 +1,6 @@
+# reload all bash scripts
+alias reload=". ~/.bashrc"
+
 # ls aliases
 alias ll='ls -hal'
 alias la='ls -A'
@@ -6,18 +9,23 @@ alias l='ls -CF'
 # cd aliases
 alias ~='cd ~'
 alias ..='cd ..'
+alias cdd='cd -'
+
+alias cdgems='cd /opt/local/lib/ruby/gems/1.8/gems/'
+alias cdgems1.9='cd /opt/local/lib/ruby1.9/gems/1.9.1/gems/'
 
 # resources aliases
 alias free='free -m'
 alias dus='du -Pschx'
 
-# shell aliases
-alias reload=". ~/.bash_profile"
+# tar aliases
 alias guntar='tar -xzvlf'
 alias gtar='tar -zcvf'
-alias gems="cd /opt/local/lib/ruby/gems/1.8/gems/"
-alias gems1.9="cd /opt/local/lib/ruby1.9/gems/1.9.1/gems/"
 
 # MySQL via MacPorts
 # Usage == mysql.server help
 alias mysql.server='/opt/local/share/mysql5/mysql/mysql.server'
+
+# top aliases
+alias tu="top -o cpu"   # cpu
+alias tm="top -o vsize" # memory
