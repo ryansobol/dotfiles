f4bd20a188e440ea0c3787eff34ef97ba223b6a4
Ryan Sobol <contact@ryansobol.com>
2010-07-06 11:34:52 -0500

    irbrc.rb   * Refactored for readability

--- irbrc.rb
+++ irbrc.rb
@@ -1,5 +1,11 @@
-script_console_running = ENV.include?('RAILS_ENV') && IRB.conf[:LOAD_MODULES] && IRB.conf[:LOAD_MODULES].include?('console_with_helpers')
-rails_running          = ENV.include?('RAILS_ENV') && !(IRB.conf[:LOAD_MODULES] && IRB.conf[:LOAD_MODULES].include?('console_with_helpers'))
+script_console_running = ENV.include?('RAILS_ENV') &&
+                         IRB.conf[:LOAD_MODULES] &&
+                         IRB.conf[:LOAD_MODULES].include?('console_with_helpers')
+
+rails_running          = ENV.include?('RAILS_ENV') &&
+                         !(IRB.conf[:LOAD_MODULES] &&
+                         IRB.conf[:LOAD_MODULES].include?('console_with_helpers'))
+
 irb_standalone_running = !script_console_running && !rails_running
 
 if script_console_running
