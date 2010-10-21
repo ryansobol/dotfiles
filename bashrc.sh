598f0d2e4264d5c07b5ed461e542440e7446968f
Ryan Sobol <contact@ryansobol.com>
2010-10-20 19:51:55 -0700

    bash/darwin/functions.sh   * Added a trash() function bashrc.sh   * Sources functions.sh under Darwin

--- bashrc.sh
+++ bashrc.sh
@@ -5,4 +5,5 @@ source ~/bin/dotfiles/bash/aliases.sh
 if [[ $OSTYPE == "darwin10.0" ]]; then
   source ~/bin/dotfiles/bash/darwin/env.sh
   source ~/bin/dotfiles/bash/darwin/aliases.sh
+  source ~/bin/dotfiles/bash/darwin/functions.sh
 fi
