f57c70f3ad6db03b9433ec062aa4a1c26163940d
Ryan Sobol <contact@ryansobol.com>
2011-02-03 14:45:11 -0800

    bash/darwin/aliases.sh   * Refactored mys alias and usage comment   * Added pgs alias and usage comment

--- bash/darwin/aliases.sh
+++ bash/darwin/aliases.sh
@@ -10,8 +10,12 @@ alias cpwd="pwd | pbcopy"
 ## MacPorts
 
 # MySQL aliases
-# Usage: myd help
-alias mys='/opt/local/share/mysql5/mysql/mysql.server'
+# Usage: mys [start|stop|restart]
+alias mys="/opt/local/etc/LaunchDaemons/org.macports.mysql5/mysql5.wrapper"
+
+# PostgreSQL aliases
+# Usage: pgs [start|stop|restart]
+alias pgs="/opt/local/etc/LaunchDaemons/org.macports.postgresql84-server/postgresql84-server.wrapper"
 
 # Apache aliases
 alias apache2ctl='/opt/local/apache2/bin/apachectl'
