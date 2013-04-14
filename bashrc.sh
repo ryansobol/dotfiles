586f4bcc858eac11e4bb4e530fc4cf8eef88cbb9
Ryan Sobol <contact@ryansobol.com>
2013-04-14 13:01:04 -0700

    Update to darwin 11.4.2

--- bashrc.sh
+++ bashrc.sh
@@ -2,7 +2,7 @@ source ~/bin/dotfiles/bash/env.sh
 source ~/bin/dotfiles/bash/prompt.sh
 source ~/bin/dotfiles/bash/aliases.sh
 
-if [[ $OSTYPE == "darwin11.3.0" ]]; then
+if [[ $OSTYPE == "darwin11.4.2" ]]; then
   source ~/bin/dotfiles/bash/darwin/env.sh
   source ~/bin/dotfiles/bash/darwin/aliases.sh
   source ~/bin/dotfiles/bash/darwin/functions.sh
