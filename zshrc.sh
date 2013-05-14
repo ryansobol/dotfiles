eeedd457eade8f444b1f50e7e1a3e2b8d20ce572
Ryan Sobol <contact@ryansobol.com>
2013-05-14 10:57:52 -0700

    Add gem plugin

--- zshrc.sh
+++ zshrc.sh
@@ -40,7 +40,7 @@ ZSH_THEME="robbyrussell"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git osx copydir copyfile extract)
+plugins=(copydir copyfile extract gem git osx)
 
 source $ZSH/oh-my-zsh.sh
 
