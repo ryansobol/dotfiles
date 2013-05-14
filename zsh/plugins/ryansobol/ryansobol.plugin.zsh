88ca3435a45e2dc7856a5a4c21a66573fb072c49
Ryan Sobol <contact@ryansobol.com>
2013-05-14 14:31:40 -0700

    Add ryansobol custom plugin and theme

---
+++ zsh/plugins/ryansobol/ryansobol.plugin.zsh
@@ -0,0 +1,3 @@
+dir=$(dirname $0)
+source $dir/env.sh
+source $dir/aliases.sh
--- zshrc.sh
+++ zshrc.sh
@@ -1,11 +1,13 @@
 # Path to your oh-my-zsh configuration.
 ZSH=$HOME/.oh-my-zsh
 
+ZSH_CUSTOM=~/bin/dotfiles/zsh
+
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="robbyrussell"
+ZSH_THEME="ryansobol"
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
@@ -40,11 +42,8 @@ ZSH_THEME="robbyrussell"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(copydir copyfile extract gem gitfast git-extras heroku macports osx rvm)
+plugins=(copydir copyfile extract gem gitfast git-extras heroku macports osx rvm ryansobol)
 
 source $ZSH/oh-my-zsh.sh
 
 # Customize to your needs...
-
-source ~/bin/dotfiles/zsh/env.sh
-source ~/bin/dotfiles/zsh/aliases.sh
