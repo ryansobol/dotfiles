61c44b53a8b8f1013055383e89f704c581eeec7c
Ryan Sobol <contact@ryansobol.com>
2013-05-14 11:45:57 -0700

    Add rvm plugin

--- zshrc.sh
+++ zshrc.sh
@@ -40,7 +40,7 @@ ZSH_THEME="robbyrussell"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(copydir copyfile extract gem gitfast git-extras heroku macports osx)
+plugins=(copydir copyfile extract gem gitfast git-extras heroku macports osx rvm)
 
 source $ZSH/oh-my-zsh.sh
 
