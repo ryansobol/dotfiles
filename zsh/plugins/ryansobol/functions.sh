1c1114a2f712ff3a369c253bc05b30f90328ffa1
Ryan Sobol <contact@ryansobol.com>
2013-05-18 10:04:49 -0700

    Limit and search in reverse chronological order

--- zsh/plugins/ryansobol/functions.sh
+++ zsh/plugins/ryansobol/functions.sh
@@ -1,3 +1,4 @@
 pj() {
-  cd $(find $HOME/Projects -type d -maxdepth 4 | ack -i $* | ack -v Pods --max-count=1)
+  paths=($HOME/Projects/2013 $HOME/Projects/2012 $HOME/Projects/2011)
+  cd $(find $paths -type d -maxdepth 3 | ack -i $* | ack -v Pods --max-count=1)
 }
