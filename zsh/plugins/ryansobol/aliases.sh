92ddc8c79efed0eeed0cdf2f1d774febf877e451
Ryan Sobol <contact@ryansobol.com>
2013-10-28 12:25:40 +0800

    Simply aliases

--- zsh/plugins/ryansobol/aliases.sh
+++ zsh/plugins/ryansobol/aliases.sh
@@ -1,48 +1,21 @@
-### General aliases
+### General
 
-# reload all bash scripts
+# source
 alias reload="source ~/.zshrc"
 
-# allows my bash aliases to work through sudo
-# Bash Info Manual: If the last character of the alias value is a space or tab character,
-# then the next command word following the alias is also checked for alias expansion.
-alias sudo="nocorrect sudo "
-
-# ls aliases
+# ls
 alias ll='ls -hal'
 alias la='ls -A'
 alias l='ls -CF'
 
-# cd aliases
+# cd
 alias ~='cd ~'
 
-# disk usage aliases
+# du
 alias dus='du -Pschx'
 
-### Darwin aliases
+### Darwin
 
-# top aliases
-alias tu="top -o cpu"   # cpu
-alias tm="top -o vsize" # memory
-
-## MacPorts
-
-# MySQL aliases
-# Usage: mys [start|stop|restart]
-alias mys="/opt/local/etc/LaunchDaemons/org.macports.mysql5/mysql5.wrapper"
-
-# PostgreSQL aliases
-# Usage: pgs [start|stop|restart]
-alias pgs84="/opt/local/etc/LaunchDaemons/org.macports.postgresql84-server/postgresql84-server.wrapper"
-alias pgs91="/opt/local/etc/LaunchDaemons/org.macports.postgresql91-server/postgresql91-server.wrapper"
-
-# Redis aliases
-# Usage: rds [start|stop|restart]
-alias rds="/opt/local/etc/LaunchDaemons/org.macports.redis/redis.wrapper"
-
-# git aliases
-# https://github.com/defunkt/hub
-eval "$(hub alias -s)"
-
-# Ack aliases
-alias ack="/opt/local/bin/ack-5.12"
+# top
+alias tcpu="top -o cpu"
+alias tmem="top -o vsize"
