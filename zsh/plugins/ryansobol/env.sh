92902c4f3c8fb89fa67004d88a4d0a1eac5f388a
Ryan Sobol <contact@ryansobol.com>
2013-10-28 12:31:33 +0800

    Simply env with Homebrew and rbenv

--- zsh/plugins/ryansobol/env.sh
+++ zsh/plugins/ryansobol/env.sh
@@ -1,26 +1,10 @@
+## Homebrew
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export MANPATH=/usr/local/share/man:$MANPATH
+
 ## Custom
+export PATH=$HOME/.bin:$PATH
+export EDITOR="subl -w"
 
-# set the default editor to TextMate
-export EDITOR="/usr/bin/mate -w"
-
-# append custom tools to the $PATH
-export PATH=$PATH:$HOME/.bin
-
-## Heroku Toolbelt
-
-# prepend Heroku tools to the $PATH
-export PATH=/usr/local/heroku/bin:$PATH
-
-## MacPorts
-
-# prepend MacPorts tools to the $PATH and $MANPATH
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-export MANPATH=/opt/local/man:$MANPATH
-
-# prepend PostgreSQL tools to the $PATH
-export PATH=/opt/local/lib/postgresql91/bin:$PATH
-
-## RVM
-
-# load RVM function... must invoke after all $PATH loads
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+## rbenv
+eval "$(rbenv init -)"
