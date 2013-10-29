22a7a0f953893572671e919c6399e92e1ed3b7a7
Ryan Sobol <contact@ryansobol.com>
2013-10-29 15:01:07 +0800

    Add postgres support

--- zsh/plugins/ryansobol/env.sh
+++ zsh/plugins/ryansobol/env.sh
@@ -5,6 +5,9 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH=$HOME/.bin:$PATH
 export EDITOR="subl -w"
 
+## postgres
+export PGDATA=/usr/local/var/postgres
+
 ## chruby
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 source /usr/local/opt/chruby/share/chruby/auto.sh
