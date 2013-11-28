1638f00c6bc406d5cc24e0258ba69be336fcefea
Ryan Sobol <contact@ryansobol.com>
2013-11-29 07:48:58 +0800

    Go deeper!

--- zsh/plugins/ryansobol/functions.sh
+++ zsh/plugins/ryansobol/functions.sh
@@ -1,4 +1,4 @@
 pj() {
   paths=($HOME/Projects/2013 $HOME/Projects/2012 $HOME/Projects/2011)
-  cd $(find $paths -type d -maxdepth 3 | ack -i $* | ack -v Pods --max-count=1)
+  cd $(find $paths -type d -maxdepth 4 | ack -i $* | ack -v Pods --max-count=1)
 }
