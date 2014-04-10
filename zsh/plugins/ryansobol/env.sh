b9fa50f5b1b6b983d991282a55001814637d1008
Ryan Sobol <contact@ryansobol.com>
2014-04-10 23:36:24 +0800

    Change editor to Atom

--- zsh/plugins/ryansobol/env.sh
+++ zsh/plugins/ryansobol/env.sh
@@ -3,7 +3,7 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 ## HOME
 export PATH=$HOME/.bin:$PATH
-export EDITOR="subl -w"
+export EDITOR="atom -w"
 
 ## postgres
 export PGDATA=/usr/local/var/postgres
