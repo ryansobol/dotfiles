eb047b7d98ca0e84d8194a4c0fce560619ac919f
Ryan Sobol <contact@ryansobol.com>
2014-04-15 19:50:38 +0800

    Add the ATOM_REPOS_HOME env var

--- zsh/plugins/ryansobol/env.sh
+++ zsh/plugins/ryansobol/env.sh
@@ -3,7 +3,10 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 ## HOME
 export PATH=$HOME/.bin:$PATH
+
+# Atom.io
 export EDITOR="atom -w"
+export ATOM_REPOS_HOME=~/Projects/2014/ryansobol
 
 ## postgres
 export PGDATA=/usr/local/var/postgres
