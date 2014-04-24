b01e5054de02333a5c4fbff5cd22f5e0232613bc
Ryan Sobol <contact@ryansobol.com>
2014-04-24 11:30:33 +0800

    Open atom in development mode

--- zsh/plugins/ryansobol/env.sh
+++ zsh/plugins/ryansobol/env.sh
@@ -5,7 +5,7 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH=$HOME/.bin:$PATH
 
 # Atom.io
-export EDITOR="atom -w"
+export EDITOR="atom -w -d"
 export PROJECTS=~/Projects/2014/ryansobol
 export ATOM_REPOS_HOME=$PROJECTS
 
