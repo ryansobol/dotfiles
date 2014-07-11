f3b682cd863d5bb8711cd0c6f183ee11500dfade
Ryan Sobol <contact@ryansobol.com>
2014-07-11 12:07:54 -0700

    Clean up and add history timestamps

--- zshrc.sh
+++ zshrc.sh
@@ -1,29 +1,17 @@
 ZSH=$HOME/.oh-my-zsh
 ZSH_CUSTOM=$HOME/.dotfiles/zsh
-ZSH_THEME="ryansobol"
+ZSH_THEME=ryansobol
 
 plugins=(brew gitfast ryansobol ryansobol-osx sublime)
+
+# Set a history timestamp format
+HIST_STAMPS=yyyy-mm-dd
 
 # Set to this to use case-sensitive completion
 # CASE_SENSITIVE="true"
 
-# Comment this out to disable bi-weekly auto-update checks
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment to change how often before auto-updates occur? (in days)
-# export UPDATE_ZSH_DAYS=13
-
-# Uncomment following line if you want to disable colors in ls
-# DISABLE_LS_COLORS="true"
-
-# Uncomment following line if you want to disable autosetting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
 # Uncomment following line if you want to disable command autocorrection
 # DISABLE_CORRECTION="true"
-
-# Uncomment following line if you want red dots to be displayed while waiting for completion
-# COMPLETION_WAITING_DOTS="true"
 
 # Uncomment following line if you want to disable marking untracked files under
 # VCS as dirty. This makes repository status check for large repositories much,
